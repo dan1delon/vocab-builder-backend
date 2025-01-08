5219a2ba9a37d2db5a94e5216566744f5276b366
dan1delon <143418624+dan1delon@users.noreply.github.com>
2025-01-08 12:20:21 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(
     cors({
-      origin: 'http://localhost:3000',
+      origin: 'http://localhost:3001',
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
       allowedHeaders: ['Content-Type', 'Authorization'],
       credentials: true,
