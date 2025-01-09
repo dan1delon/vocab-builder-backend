15d2afa2dde02507e2922cef6f7a51953136fbaa
dan1delon <143418624+dan1delon@users.noreply.github.com>
2025-01-09 20:48:20 +0200

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -60,24 +60,40 @@ export const loginUser = async (payload) => {
   if (!user) {
     throw createHttpError(404, 'User not found');
   }
-  const isEqual = await bcrypt.compare(payload.password, user.password);
 
+  const isEqual = await bcrypt.compare(payload.password, user.password);
   if (!isEqual) {
     throw createHttpError(401, 'Unauthorized');
   }
 
   await SessionsCollection.deleteOne({ userId: user._id });
 
+  const token = jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+      name: user.name,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: '1d' },
+  );
+
   const accessToken = randomBytes(30).toString('base64');
   const refreshToken = randomBytes(30).toString('base64');
 
-  return await SessionsCollection.create({
+  await SessionsCollection.create({
     userId: user._id,
     accessToken,
     refreshToken,
     accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
     refreshTokenValidUntil: new Date(Date.now() + ONE_DAY),
   });
+
+  return {
+    email: user.email,
+    name: user.name,
+    token,
+  };
 };
 
 export const logoutUser = async (sessionId) => {
