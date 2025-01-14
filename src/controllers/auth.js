21537b44bc6f07af4af5f6f2f3085d2ec61ce1d7
dan1delon <143418624+dan1delon@users.noreply.github.com>
2025-01-14 14:03:00 +0200

    Update auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -90,14 +90,14 @@ const setupSession = (res, session) => {
     httpOnly: true,
     expires: new Date(Date.now() + ONE_DAY),
     sameSite: 'None',
-    secure: false,
+    secure: true,
   });
 
   res.cookie('sessionId', session._id, {
     httpOnly: true,
     expires: new Date(Date.now() + ONE_DAY),
     sameSite: 'None',
-    secure: false,
+    secure: true,
   });
 };
 
